14f07a2f114786ff151fdfed7944eb4305d18a40
Thiago G. Alves <thiagogarciaalves@mail.com>
2018-10-13 14:27:29 -0300

    Adicionando um dicionário de blocos e usando no script building.py

---
+++ scripts/blocks.py
@@ -0,0 +1,7 @@
+block = {
+    "air" : 0,
+    "water" : 8,
+    "melon" : 103,
+    "tnt" : 46,
+    "ironBlock" : 42
+}
--- scripts/building.py
+++ scripts/building.py
@@ -1,14 +1,17 @@
 from mcpi.minecraft import Minecraft
+from blocks import block
+
 mc = Minecraft.create()
 pos = mc.player.getTilePos()
+
 x = pos.x
 y = pos.y
 z = pos.z
 width = 10
 heigth = 5
 length = 6
-blockType = 42
-air = 0
-mc.setBlocks(x, y, z, x + width, y + heigth, z + length, blockType)
+blockType = block["ironBlock"]
+air = block["air"]
 
+mc.setBlocks(x, y, z, x + width, y + heigth, z + length, blockType)
 mc.setBlocks(x+1, y+1, z+1, x+width-1, y+heigth-1, z+length-1, air)
